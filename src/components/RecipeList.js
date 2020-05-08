56fa401abfc666f21b2ad73b4fb702492b03a20b
zzj <zhengzijian56@gmail.com>
2020-05-08 10:41:47 +0100

    v1

---
+++ src/components/RecipeList.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react"
+import Recipe from "./Recipe"
+import { RecipeContext } from "./App"
+
+export default function RecipeList({
+  recipes,
+  // handleRecipeAdd,
+  // handleRecipeDelete,
+}) {
+  const { handleRecipeAdd } = useContext(RecipeContext)
+  return (
+    <div className="recipe-list">
+      <div>
+        {recipes.map(recipe => {
+          return (
+            <Recipe
+              key={recipe.id}
+              {...recipe}
+              // handleRecipeDelete={handleRecipeDelete}
+            ></Recipe>
+          )
+        })}
+      </div>
+      <div className="recipe-list__add-recipe-btn-container">
+        <button className="btn btn--primary" onClick={handleRecipeAdd}>
+          Add Recipe
+        </button>
+      </div>
+    </div>
+  )
+}
